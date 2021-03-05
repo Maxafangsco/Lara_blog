5a2e4b9d8baeba403dff954073e87ba79919f014
Maxafangsco <39309700+Maxafangsco@users.noreply.github.com>
2021-03-05 13:16:43 +0400

    first commit loading project

---
+++ resources/views/about.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.home')
+
+ @section('content')
+
+<div class="conatiner">
+<div class="row">
+            <div class="col-6">
+                        <div class="section-title mt-20">
+                                    <h5>About us</h5>
+                                    <h2>Welcome to SRA </h2>
+                                </div> <!-- section title -->
+                                <div class="about-cont">
+                                
+                                    <p>Quality health care remains a luxury for most of Africa and Cameroon in particular. We die of preventable causes partly because little to nothing is known on disease patterns in our community. We belief, standards of care can be improved with evidence-based data on the prevailing health concerns in every community.</p>
+                                    <p>In this light the “Sintieh Research Academy” has decided to aid in training persons  in the field of Scientific research, focusing on health. The academy will provide the foundation needed to build a formidable career in research; we do not seek to just submit project but to build prolific scientific researchers. This initiative seeks to train youths in the basics of health research and to empower these trainees lead research projects in their respective communities. We look forward to creating a database of scientific information which can be used as information to develop protocols for management of outbreaks and endemic diseases. In the long term our growing scientific community will lead innovative, research to champion discoveries in our time.</p>
+                                    <p>Improve the standard of living in Cameroon by training leaders in scientific research. We look forward in aiding in the attainment of sustainable development goals for: Good health and wellbeing (3), Quality education (4), industry innovation and infrastructure (9), and reducing inequalities (10).</p>
+                        </div>
+
+            </div>
+            <div class="col-6">
+            
+            <div class="about-image mt-20">
+                        <!-- <img src="img/image_1.jpg" alt="About" > -->
+                    </div>  <!-- about imag -->
+                            
+            </div>
+
+        
+</div>
+</div>
+
+@endsection
---
+++ resources/views/test.blade.php
@@ -0,0 +1,67 @@
+          
+    <div class="upcoming_event">
+
+<div class="col-6 bg-danger">
+    <div class="event_left bg-scondary">
+    <div class="section-title mt-50">
+      <h5>About us</h5>
+      <h2>Welcome to SRA</h2>
+    </div>
+    <!-- section title -->
+    <div class="about-cont">
+      <p>
+        Quality health care remains a luxury for most of Africa and Cameroon in particular.
+         We die of preventable causes partly because little to nothing is known on disease patterns in our community. <br />
+        <br />
+         We belief, standards of care can be improved with evidence-based data on the prevailing health concerns in every community.
+      </p>
+      <a href="about.html" class="main-btn mt-55">Learn More</a>
+    </div>
+  </div>
+    </div>
+</div>
+
+
+
+<div class="col-6 bg-warning">
+
+  <!-- background image here -->
+  <img src="/img/bg-1.png" alt="background image">
+
+    <div class="event_right">
+    <div class="event-title">
+        <h3>Upcoming events</h3>
+      </div>
+      <!-- event title -->
+      <ul>
+        <li>
+          <div class="singel-event">
+            <span><i class="fa fa-calendar"></i> 2 March 2021</span>
+            <a href="events-singel.html"><h4>Research workshop</h4></a>
+            <span><i class="fa fa-clock-o"></i> 10:00 Am - 3:00 Pm</span>
+            <span><i class="fa fa-map-marker"></i> Online</span>
+          </div>
+        </li>
+        <li>
+          <div class="singel-event">
+            <span><i class="fa fa-calendar"></i> 2 July 2021</span>
+            <a href="events-singel.html"><h4>Leadership Summit</h4></a>
+            <span><i class="fa fa-clock-o"></i> 10:00 Am - 3:00 Pm</span>
+            <span><i class="fa fa-map-marker"></i> University of Buea Compus </span>
+          </div>
+        </li>
+        <li>
+          <div class="singel-event">
+            <span><i class="fa fa-calendar"></i> 2 May 2021</span>
+            <a href="events-singel.html"><h4>Enviroement conference</h4></a>
+            <span><i class="fa fa-clock-o"></i> 10:00 Am - 3:00 Pm</span>
+            <span><i class="fa fa-map-marker"></i>  Onlin/span>
+          </div>
+        </li>
+      </ul>
+    </div>
+    </div>
+
+</div>
+
+</div>
